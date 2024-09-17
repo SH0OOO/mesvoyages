07bee2e00c353134c5f2742e0c334bed85174b7d
Maxime <maximedelorme38200@gmail.com>
2024-09-17 10:38:00 +0200

    Phase 2 : premier test "Hello world !"

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Description of AccueilController
+ *
+ * @author mamax
+ */
+class AccueilController {
+    #[Route('/', name: 'acceuil')]
+    public function index(): Response{
+        return new Response ('hello world !');
+    }
+}
